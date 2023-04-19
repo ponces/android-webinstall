219e833d31c8b882f69739793405feca235a4b57
Alberto Ponces <ponces26@gmail.com>
2023-04-19 14:53:47 +0100

    config: Adapt to AOSP GSI

--- src/config.js
+++ src/config.js
@@ -3,28 +3,8 @@ export default {
     ACCENT_COLOR: "#0366d6",
     SUPPORTED_DEVICES: [
         {
-            name: "Pixel 4",
-            model: "flame",
-        },
-        {
-            name: "Pixel 4 XL",
-            model: "coral",
-        },
-        {
-            name: "Pixel 4a 5G",
-            model: "bramble",
-        },
-        {
-            name: "Pixel 5",
-            model: "redfin",
-        },
-        {
-            name: "Pixel 6",
-            model: "oriole",
-        },
-        {
-            name: "Pixel 6 Pro",
-            model: "raven",
+            name: "Xiaomi Mi 11 Lite 5G",
+            model: "renoir",
         },
     ],
     RELEASE_VARIANTS: {
@@ -37,30 +17,22 @@ export default {
                 "Version with Google services included, so the apps you’re familiar with will work. Recommended for most users.",
             suffix: " with Google services",
         },
-        test: {
-            description: "Experimental version for testing only.",
-            suffix: "experimental",
-        },
-        beta: {
-            description: "Beta version for testing only.",
-            suffix: "beta",
-        },
     },
     DONATION_LINKS: [
         {
             title: "Recurring donation",
             description:
-                "Please consider making a recurring donation on Patreon for benefits such as early access to updates, exclusive behind-the-scenes development news, and priority support. This allows us to support the project sustainably in the future.",
+                "Please consider making a recurring donation on Github. This allows me to support the project sustainably in the future.",
             highlight: true,
-            url: "https://patreon.com/kdrag0n",
-            icon: "patreon",
+            url: "https://github.com/sponsors/ponces",
+            icon: "github",
         },
         {
             title: "One-time donation",
             description:
                 "You can also make a one-time donation through PayPal. It doesn’t help with sustainable support, but every little bit is appreciated!",
             highlight: false,
-            url: "https://paypal.me/kdrag0ndonate",
+            url: "https://paypal.me/aponces",
             icon: "paypal",
         },
     ],
--- src/core/devices.js
+++ src/core/devices.js
@@ -1,19 +1,5 @@
 const DEVICE_NAMES = {
-    sailfish: "Pixel",
-    marlin: "Pixel XL",
-    walleye: "Pixel 2",
-    taimen: "Pixel 2 XL",
-    blueline: "Pixel 3",
-    crosshatch: "Pixel 3 XL",
-    sargo: "Pixel 3a",
-    bonito: "Pixel 3a XL",
-    flame: "Pixel 4",
-    coral: "Pixel 4 XL",
-    sunfish: "Pixel 4a",
-    bramble: "Pixel 4a 5G",
-    redfin: "Pixel 5",
-    oriole: "Pixel 6",
-    raven: "Pixel 6 Pro",
+    renoir: "Xiaomi Mi 11 Lite 5G",
 };
 
 export function getDeviceName(device) {
